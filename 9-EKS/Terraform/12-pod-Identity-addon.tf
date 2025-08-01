61e2eddeffd43742616cc93b53dac55990fef91a
Snowpiercer <113368634+elkhayati-yasser@users.noreply.github.com>
2025-08-01 18:48:36 +0100

    typo

--- 9-EKS/Terraform/12-pod-Identity-addon.tf
+++ 9-EKS/Terraform/12-pod-Identity-addon.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_addon" "pod_identity" {
    cluster_name = aws_eks_cluster.eks.name
    addon_name = "eks-pod-identity-agent"
-   addon_verion = "v1.2.0-eksbuild.1"
+   addon_version = "v1.2.0-eksbuild.1"
 
 }
 
